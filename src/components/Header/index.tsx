25ace0d46dc14e65c23111f4d03e5024b8163f77
David Bernardo <sdavi2840@gmail.com>
2021-04-23 12:05:59 -0300

    github

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,19 @@
-import styles from './styles.module.scss';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
-export function Header(){
-    const currentDate = format(new Date(), 'EEEE, d / MMMM', {
-        locale: ptBR
-    })
-    return (
-        <header className={styles.headContainer}>
-            <img src='/logo.svg' alt="Podcastr" />
-            <p>O melhor para você ouvir, sempre</p>
-            <span>{currentDate}</span>
+import styles from './styles.module.scss';
 
-        </header>
-    )
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Logo"/>
+
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
+    </header>
+  );
 }
--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,41 +1,84 @@
+import Image from 'next/image';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+
 import styles from './styles.module.scss';
 
-export function Player(){
-    return (
-        <div className={styles.playerContainer}>
-            <header>
-                <img src="/playing.svg" alt="Tocando Agora" />
-                <strong>Tocando agora</strong>
-            </header>
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
-            <footer className={styles.empty}>
-                <div className={styles.progress}>
-                    <span>00:00</span>
-                    <div className={styles.slider}>
-                        <div className={styles.emptySlider}/>
-                    </div>
-                    <span>00:00</span>
-                </div>
-                <div className={styles.buttons}>
-                    <button type="button">
-                        <img src="/shuffle.svg" alt="Embaralhar" />
-                    </button>
-                    <button type="button">
-                        <img src="/play-previous.svg" alt="Anterior" />
-                    </button>
-                    <button type="button" className={styles.playButton}>
-                        <img src="/play.svg" alt="Tocar" />
-                    </button>
-                    <button type="button">
-                        <img src="/play-next.svg" alt="Próximo" />
-                    </button>
-                    <button type="button">
-                        <img src="/repeat.svg" alt="Repetir" />
-                    </button>
-                </div>
-            </footer>
+export function Player() {
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+
+  const episode = episodeList[currentEpisodeIndex];
+
+  return (
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora"/>
+        <strong>Tocando agora</strong>
+      </header>
+
+      { episode ? (
+        <div className={styles.currentEpisode}>
+          <Image 
+            width={592} 
+            height={592} 
+            src={episode.thumbnail} 
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
         </div>
-    )
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      ) }
+
+      <footer className={!episode ? styles.empty : ''}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            { episode ? (
+              <Slider 
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff'}}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4}}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            ) 
+          }
+          </div>
+          <span>00:00</span>
+        </div>
+
+        { episode && (
+          <audio 
+            src={episode.url}
+            autoPlay
+          />
+        )}
+
+        <div className={styles.buttons}>
+          <button type="button" disabled={!episode}>
+            <img src="/shuffle.svg" alt="Embaralhar" />
+          </button>
+          <button type="button" disabled={!episode}>
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button type="button" className={styles.playButton} disabled={!episode}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+          <button type="button" disabled={!episode}>
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button type="button" disabled={!episode}>
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
+    </div>
+  );
 }
